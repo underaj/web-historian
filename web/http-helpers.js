49ea7fd3c7c2866e4c5cc86b7b220d9d9e51cb38
underaj <chingyang91@gmail.com>
2016-06-29 12:32:38 -0700

    Refactoring request handler to helper

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
-
+var ROOTPATH = __dirname + '/public';
 exports.headers = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -14,6 +14,14 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  var statusCode = 200;
+  fs.readFile(ROOTPATH + asset, function(err, data) {
+    if (err) {
+      callback(res, '', 404);
+    } else {
+      callback(res, data, statusCode);
+    }
+  });
 };
 
   // Write some code here that helps serve up your static files!
@@ -21,3 +29,9 @@ exports.serveAssets = function(res, asset, callback) {
   // css, or anything that doesn't change often.)
 
 // As you progress, keep thinking about what helper functions you can put here!
+
+exports.sendResponse = function(response, data, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers['']);
+  response.end(data);
+};
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,43 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var fs = require('fs');
+var ROOTPATH = __dirname + '/public';
+var helpers = require('./http-helpers');
+var fileType = require('file-type');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var urlParse = url.parse(req.url);
+  // console.log(urlParse);
+
+  if (urlParse.pathname === '/') {
+    helpers.serveAssets(res, '/index.html', helpers.sendResponse);
+  } else {
+    helpers.serveAssets(res, urlParse.pathname, helpers.sendResponse);
+  }
+  // if ( req.method === 'GET' ) {
+  //   if ( urlParse.pathname === '/' ) {
+  //     statusCode = 200;
+  //     fs.readFile( __dirname + '/public/index.html', function(err, data) {
+  //       header = {'Content-Type': 'text/html'};
+  //       res.writeHead(statusCode, header);
+  //       res.write(data);
+  //       res.end();
+  //     });
+  //   } else {
+  //     fs.readFile( ROOTPATH + req.url, function(err, data) {
+  //       if (err) {
+  //         res.writeHead(404, header);
+  //         res.end();
+  //       } else {
+  //         header = {'Content-Type': 'text/css'};
+  //         statusCode = 200;
+  //         res.writeHead(statusCode, header);
+  //         res.write(data);
+  //         res.end();
+  //       }
+  //     });
+  //   }
+  // }
 };
